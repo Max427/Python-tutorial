dfbc50ff1ae62645e33f0bee7dee5d952401a4c5
Max <96799241+Max427@users.noreply.github.com>
2024-03-10 21:51:56 +0000

    learned how multiple assignment works

---
+++ Day 1/multiple-assignment.py
@@ -0,0 +1,16 @@
+# name = "Max"
+# age = 23
+# attractive = True
+
+# name, age, attractive = "Max", 23, True
+
+# print(name)
+# print(age)
+# print(attractive)
+
+Spongebob = Patrick = Sandy = Squidward = 30
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidward)
