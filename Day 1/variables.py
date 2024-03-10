a1ce515ffac0e87fcf323200540bf656ee03766c
Max <96799241+Max427@users.noreply.github.com>
2024-03-10 21:46:34 +0000

    learned basics of variables, added "variables.py"

---
+++ Day 1/variables.py
@@ -0,0 +1,17 @@
+# firstName = "Max"
+# lastName = "Code"
+# fullName = firstName + " " + lastName
+# print("Hello " + fullName)
+
+# age = 22
+# age += 1
+# print("Your age is: " +str(age))
+# print(type(age))
+
+# height = 250.5
+# print("Your height is: "+str(height)+ "cm")
+# print(type(height))
+
+human = True
+# print(type(human))
+print("Are you a human: "+str(human))
