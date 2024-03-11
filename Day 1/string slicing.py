03b8252e8752ca416e6e0496f4ed4adcff21961b
Max <96799241+Max427@users.noreply.github.com>
2024-03-11 00:14:14 +0000

    learned basic string slicing

---
+++ Day 1/string slicing.py
@@ -0,0 +1,16 @@
+# name = "Max Code"
+
+# firstName = name[:3]
+# lastName = name[4:]
+# funkyName = name[::2]
+# reversedName = name[::-1]
+
+# print(reversedName)
+
+website1 = "http://google.com"
+website2 = "http://wikipedia.com"
+
+slice = slice(7,-4)
+
+print(website1[slice])
+print(website2[slice])
