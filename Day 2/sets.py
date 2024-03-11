e0a22d44667b74693ff17994840ab59be0ad6a52
Max <96799241+Max427@users.noreply.github.com>
2024-03-11 19:37:05 +0000

    learned how a set works

---
+++ Day 2/sets.py
@@ -0,0 +1,15 @@
+utensils = {"fork", "spoon", "knife"}
+dishes = {"bowl", "plate", "cup", "knife"}
+
+# utensils.add("napkin")
+# utensils.remove("fork")
+# utensils.clear()
+# dishes.update(utensils)
+# dinnerTable = utensils.union(dishes)
+
+# print(dishes.difference(utensils))
+print(utensils.intersection(dishes))
+
+# for x in dinnerTable:
+#     print(x)
+
