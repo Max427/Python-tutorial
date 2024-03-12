18d9fbaeaf256f1d31cc851fd2255b851389314d
Max <96799241+Max427@users.noreply.github.com>
2024-03-12 21:57:29 +0000

    learned how functions work

---
+++ Day 3/functions.py
@@ -0,0 +1,10 @@
+def hello(first_name, last_name, age):
+    print("Hello " + first_name + " " + last_name)
+    print("You are " + str(age) + " years old")
+    print("Have a nice day!")
+
+# my_name = "Max"
+# # hello("Max")
+# hello(my_name)
+    
+hello("Max","Code", 23)
